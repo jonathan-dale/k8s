a8487262115f70c0a29671a04852da1af88d5dd5
jonathan-dale <dalej1846@gmail.com>
2021-07-02 12:32:04 -0400

    dont use functions, too messy to read

--- install/ubuntu/ubuntu-install.bash
+++ install/ubuntu/ubuntu-install.bash
@@ -28,10 +28,9 @@ function info {
 }
 
 
-# timekeeper
 START_TIME=`date +%M%S`
 
-# trap errors for debuggung ***NOTE: will not work with `set -e` option***
+# trap errors for debugging ***NOTE: will not work with `set -e` option***
 trap abort ERR
 
 
@@ -39,80 +38,72 @@ info "checking if user is root"
 AMIROOT=`id -u`
 [[ "$AMIROOT" == 0 ]] || die "must be root to run this..." 2
 
-# install junk
 info "installing required packages (apt-transport-https, ca-certificates, curl, software-properties-common, gnupg2)"
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
 
 
-function install_containerd {
-  cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
-  overlay
-  br_netfilter
-EOF
-  
-  sudo modprobe overlay
-  sudo modprobe br_netfilter
-  
-  # Setup required sysctl params, these persist across reboots.
-  cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-  net.bridge.bridge-nf-call-iptables  = 1
-  net.ipv4.ip_forward                 = 1
-  net.bridge.bridge-nf-call-ip6tables = 1
-EOF
-  
-  # Apply sysctl params without reboot
-  sudo sysctl --system
-
-
-  ## Add Docker's official GPG key
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
-  
-  ## Add Docker apt repository.
-  ## *** Find the correct ubuntu distro with $(lsb_release -cs) (e.g. focal, bionic...)
-  sudo add-apt-repository \
-      "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) \
-      stable"
-  
-  ## Install containerd
-  sudo apt-get update && sudo apt-get install -y containerd.io
-
-  # Configure containerd
-  sudo mkdir -p /etc/containerd
-  sudo containerd config default | sudo tee /etc/containerd/config.toml
-  
-  # Restart containerd
-  sudo systemctl restart containerd
-} 
-
-
-
-function install_kubernetes {
-  # add K8's GPG key
-  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-  
-  # add K8's repo
-  cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-  deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-  
-  # update and install bins 
-  ## we use version 1.19.x.xx here so we can do an update later,
-  sudo apt-get update
-  sudo apt-get install -y kubelet=1.19.0-00 kubeadm=1.19.0-00 kubectl=1.19.0-00
-  sudo apt-mark hold kubelet kubeadm kubectl
-  
-  # add ip tables and enable immediately
-  echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
-  sudo sysctl -p
-}
-  
 info "instaling containerd runtime"
-install_containerd
-info "installing kubernetes packages"
-install_kubernetes
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+  
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
-# awesome!
+# Setup required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+## Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
+
+## Add Docker apt repository.
+## *** Find the correct ubuntu distro with $(lsb_release -cs) (e.g. focal, bionic...)
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+## Install containerd
+sudo apt-get update && sudo apt-get install -y containerd.io
+
+# Configure containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+# Restart containerd
+sudo systemctl restart containerd
+
+
+info "installing kubernetes packages"
+# add K8's GPG key and repo
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+  
+info "update and install bins, locking in at version 1.19.x.xx here so we can do an update later"
+sudo apt-get update
+sudo apt-get install -y kubelet=1.19.0-00 kubeadm=1.19.0-00 kubectl=1.19.0-00
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# Use completion and 'k' simlink
+ln -s /usr/local/bin/kubectl /usr/local/bin/k
+echo 'source /usr/share/bash-completion/bash_completion' >> /home/ubuntu/.bashrc
+echo 'source <(kubectl completion bash)' >> /home/ubuntu/.bashrc
+echo 'complete -F __start_kubectl k' >> /home/ubuntu/.bashrc
+
+# add ip tables and enable immediately
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
 cat <<EOF
 
      Awesome!
